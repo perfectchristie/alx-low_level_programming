662b22ed89310b1d3ad91237b2d5b74b71f9e5d3
Perfection <adechristie2004@gmail.com>
2022-07-04 23:23:52 -0700

    +ve or -ve

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -8,9 +8,9 @@
  */
 int main(void)
 {
-	int n;
+int n;
 srand(time(0));
-n = rand() - RAND_MAX / 2
+n = rand() - RAND_MAX / 2;
 if (n == 0)
 {
 	printf("%i is zero\n", n);
@@ -24,4 +24,4 @@ else if (n > 0)
 	printf("% is positive\n", n);
 }
 Return (0)
-}
+	}

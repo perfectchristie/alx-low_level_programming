ed71b93acf91b87c3a908bdcb2fca051bdf8dbf4
Perfection <adechristie2004@gmail.com>
2022-07-05 19:33:35 -0700

    is lower

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -13,5 +13,5 @@ void print_alphabet(void)
 		_putchar(j);
 	}
 	_putchar('\n');
-};
+}
 {
---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/**
+ * _islower -> checks if the character is lowercase
+ * Return: returns 1 or 0 depending on the condition
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}

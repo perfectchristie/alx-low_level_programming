abfaa6cb6fda53d7cab900a4ced4828b9a2587be
Perfection <adechristie2004@gmail.com>
2022-07-06 00:39:07 -0700

    last digit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_last_digit -> prints the last digit
+ *
+ * Return: the last digit
+ */
+int print_last_digit(int n)
+{
+	int x;
+	print_last_digit(98);
+	print_last_digit(0);
+	x = print_last_digit(-1024);
+	_putchar('0' + x);
+	_putchar('\n');
+	return (0);
+}

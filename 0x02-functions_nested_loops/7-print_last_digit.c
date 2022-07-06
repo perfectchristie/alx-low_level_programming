5388183238b771afb247caf82cf09087f1df412e
Perfection <adechristie2004@gmail.com>
2022-07-06 02:24:22 -0700

    last digit

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -6,13 +6,13 @@
  * Return: the last digit
  */
 int print_last_digit(int n)
-(n) = 98, 0, -1024
 {
 	int x;
+	x = n % 10
 	print_last_digit(98);
 	print_last_digit(0);
 	x = print_last_digit(-1024);
 	_putchar('0' + x);
 	_putchar('\n');
-	return (0);
+	return (x);
 }

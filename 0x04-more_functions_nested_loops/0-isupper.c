26ec8cc03a2c8d4f7447bd00d92ed3d2b3576b80
Perfection <adechristie2004@gmail.com>
2022-07-08 21:13:20 -0700

    is upper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _isupper -> to check upper case
+ * @c: integer to be checked
+ * Return: 1 for uppercase but 0 otherwise
+ */
+int _isupper(int c);
+{
+	return (c >= 'A' && c <= 'Z');
+}

a7d856f98d5917cb14f0b15b170bea0d5343c8af
Perfection <adechristie2004@gmail.com>
2022-07-12 08:48:59 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -1,9 +1,9 @@
 #include "main.h"
-#include <stdio.h>
+#include <string.h>
 
 /**
  * _strlen -> to find the length of a string
- * @s: string pointer to point to this function
+ * @s: string pointer to point to this function<F11>
  * Return: the length of the string
  */
 

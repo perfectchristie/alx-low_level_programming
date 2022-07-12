2feb300b1292380a36af9e08b43f15ab8412880f
Perfection <adechristie2004@gmail.com>
2022-07-12 08:51:37 -0700

    length

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,15 +2,13 @@
 #include <string.h>
 
 /**
- * _strlen -> to find the length of a string
+ * _strlen -> function to find the length of a string
  * @s: string pointer to point to this function<F11>
  * Return: the length of the string
  */
-
 int _strlen(char *s)
 {
 	int len;
-
 	for (; *s != '\0'; s++)
 	{
 		len += 1;
